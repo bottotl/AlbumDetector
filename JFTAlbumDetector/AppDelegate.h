9661cf7dbf07f14b409028124751382eb76518e3
syfll <lintao.yu@dianping.com>
2017-11-04 14:11:54 +0800

    Initial Commit

---
+++ JFTAlbumDetector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFTAlbumDetector
+//
+//  Created by syfll on 2017/11/4.
+//  Copyright © 2017年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFTAlbumDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFTAlbumDetector
+//
+//  Created by syfll on 2017/11/4.
+//  Copyright © 2017年 syfll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
